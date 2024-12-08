e888afa357e4f2a0b2054ecde6f09c5b402fa16f
Goutam <goutambyte@gmail.com>
2024-12-08 20:01:01 +0530

    Cache related changes

--- src/main/java/com/redis_poc/service/UserService.java
+++ src/main/java/com/redis_poc/service/UserService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.redis_poc.entity.User;
@@ -16,11 +19,14 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Cacheable(value = "users", key = "#id")
 	public Optional<User> getUserById(UUID id) {
 		Optional<User> user = userRepository.findById(id);
 		return user;
 	}
 
+	@Caching(evict = { @CacheEvict(value = "users", key = "#user.id"),
+			@CacheEvict(value = "users", allEntries = true) })
 	public User createUser(User user) {
 		if (user.getId() == null) {
 			user.setId(UUID.randomUUID());
@@ -28,10 +34,12 @@ public class UserService {
 		return userRepository.save(user);
 	}
 
+	@Cacheable(value = "users")
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
 
+	@CacheEvict(value = "users", key = "#id")
 	public void deleteUser(UUID id) {
 		userRepository.deleteById(id);
 	}
